2f6ab3aaa0a98eeaa14eb757e994991c88fc0a98
PabloGM1204 <pgarmun203@g.educaand.es>
2023-10-20 11:10:10 +0200

    Reorganización y creación de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'runes',
+    loadChildren: () => import('./pages/runes/runes.module').then( m => m.RunesPageModule)
   },
 ];
 
